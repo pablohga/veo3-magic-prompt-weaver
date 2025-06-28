4a1ea9d8070fb76f2fc238e46c123cb76a488238
Pabo Azevedo <pablohga@gmail.com>
2025-06-28 21:40:57 +0100

    `Update vite.config.ts: change server port from 8080 to 3001`

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3001,
+    /* port: 8080, */
   },
   plugins: [
     react(),
